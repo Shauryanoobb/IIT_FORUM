3034a5371757e5e8a44e7abfebbed1a09a793557
Aviral-Sharma-2578 <117987884+Aviral-Sharma-2578@users.noreply.github.com>
2024-08-29 21:35:32 +0530

    Update route.js

--- src/app/api/Users/route.js
+++ src/app/api/Users/route.js
@@ -1,1 +1,28 @@
+import User from "@/app/models/users";
+import { NextResponse } from "next/server";
 
+export async function GET() {
+  try {
+    const users = await User.find();
+    //console.log(users);
+    return Response.json(users);
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
+
+export async function POST(req) {
+  try {
+    console.log("lol");
+    const body = await req.json();
+    console.log("problem");
+    const userData = body.formData;
+
+    await User.create(userData);
+
+    return NextResponse.json({ message: "user Created" }, { status: 201 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  }
+}
