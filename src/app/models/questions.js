458a8d3dd665b376a14b96e45a6385757b857988
Your Name <you@example.com>
2024-09-25 18:28:27 +0530

    message

---
+++ src/app/models/questions.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.Promise = global.Promise;
+
+const QuestionSchema = new Schema({
+    is_anonymous: { type: Boolean, default: false },
+    user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    title: { type: String, required: true },
+    question_body:  { type: String, required: true },
+    tags: { type: String, enum: ['IIT Bombay', 'IIT Delhi', 'IIT Kanpur', 'IIT Kharagpur', 'IIT Madras', 'IIT Roorkee', 'IIT Indore'], required: true },
+    answer: [
+        { mentor_id: Schema.Types.ObjectId, text: String }
+    ],
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+    share_count: { type: Number, default: 0 },
+    view_count:  { type: Number, default: 0 },
+}, { timestamps: true });
+
+const Question = models.Question || model("Question", QuestionSchema);
+
+export default Question;
